50bbce8b516b6c6a1e972252fd72e363120e47ce
Habib Amami <habibamami050@gmail.com>
2025-08-01 15:17:55 +0100

    theme switcher component

---
+++ components/theme-switcher.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React from "react";
+import { LaptopMinimalIcon, MoonIcon, SunIcon } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
+
+export default function ThemeSwitcher() {
+  const [open, setOpen] = React.useState(false);
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <DropdownMenu open={open} onOpenChange={setOpen}>
+      <DropdownMenuTrigger asChild>
+        <Button size="icon" variant="outline">
+          <SunIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onSelect={() => setTheme('light')}>
+          <SunIcon /> Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onSelect={() => setTheme('dark')}>
+          <MoonIcon /> Dark
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
