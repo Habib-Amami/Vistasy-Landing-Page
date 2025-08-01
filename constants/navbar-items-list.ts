140f37264a245495b3f372e2009a8fd656625ff7
Habib Amami <habibamami050@gmail.com>
2025-08-01 15:17:43 +0100

    constants navbar items list

---
+++ constants/navbar-items-list.ts
@@ -0,0 +1,7 @@
+export const NavBarItems = [
+    { name: 'home', href: '/home' },
+    { name: 'benefits', href: '/benefits' },
+    { name: 'solution', href: '/solution' },
+    { name: 'pricing', href: '/pricing' },
+    { name: 'contact', href: '/contact' },
+]
